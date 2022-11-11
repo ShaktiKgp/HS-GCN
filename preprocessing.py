3375bf7d42759ff92e772991a669c3a51a54f96b
Shakti Prasad <71284983+ShaktiKgp@users.noreply.github.com>
2022-11-11 20:27:25 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 import pickle
 import scipy.sparse as sparse
+import gc
 
-dat_f = open('./raw_data/ratings.csv', encoding='utf-8')
-sentimentList = []
-for line in dat_f:
-    s = line.strip().split(',')
-    sentimentList.append(s)
-dat_f.close()
-data = pd.DataFrame(sentimentList).iloc[:, :3]
+######Loading Data........
+
+data = pd.read_csv('./raw_data/ratings.csv', encoding='utf-8').iloc[:, :3]
+# print(data)
 data.columns = ['user_id', 'item_id', 'ratings']
 print('data loaded...')
 
@@ -21,17 +18,17 @@ def get_count(tp, key):
     return count
 
 
-MIN_USER_COUNT = 1
-MIN_ITEM_COUNT = 1
+min_user = 1
+min_item = 1
 
 
-def filter_triplets(tp, min_uc=MIN_USER_COUNT, min_ic=MIN_ITEM_COUNT):
-    """Only keep the triplets for items which were rated by at least min_ic users."""
+def filter_triplets(tp, min_uc=min_user, min_ic=min_item):
+    
+    # Items which are atleast rated once.
     _item_count = get_count(tp, 'item_id')
     tp = tp[tp['item_id'].isin(_item_count.index[_item_count >= min_ic])]
-    '''Only keep the triplets for users who listened to at least min_uc songs
-    After doing this, some of the songs will have less than min_uc users, 
-    but should only be a small proportion'''
+
+    # users who listened to at least min_uc songs.
     _user_count = get_count(tp, 'user_id')
     tp = tp[tp['user_id'].isin(_user_count.index[_user_count >= min_uc])]
     _item_count = get_count(tp, 'item_id')
@@ -71,7 +68,10 @@ uid_name = df_gp.size().index
 
 def split_train_test(tp_rating):
     n_ratings = tp_rating.shape[0]
+
+    #70-30 train-test split 
     test = np.random.choice(n_ratings, size=int(0.30 * n_ratings), replace=False)
+
     test_idx = np.zeros(n_ratings, dtype=bool)
     test_idx[test] = True
     _tp_test = tp_rating[test_idx]
@@ -83,7 +83,6 @@ edge_index = np.empty(shape=[0, 2], dtype=int)
 train_ui = np.empty(shape=[0, 2], dtype=int)
 test_ui = np.empty(shape=[0, 2], dtype=int)
 
-print('edge_index train_ui test_ui started...')
 for name in uid_name:
     tp_train, tp_test = split_train_test(df_gp.get_group(name))
     tr_ui = np.array(tp_train[['user_id', 'item_id']])
@@ -94,9 +93,7 @@ for name in uid_name:
     edge_index = np.append(edge_index, re_edge, axis=0)
     train_ui = np.append(train_ui, tr_ui, axis=0)
     test_ui = np.append(test_ui, te_ui, axis=0)
-print('edge_index train_ui test_ui finished...')
 
-print('train_matrix test_matrix started...')
 row = train_ui[:, 0]
 col = train_ui[:, 1]
 data = np.ones_like(row)
@@ -105,9 +102,60 @@ row = test_ui[:, 0]
 col = test_ui[:, 1]
 data = np.ones_like(row)
 test_matrix = sparse.coo_matrix((data, (row, col)), shape=(user_num, item_num), dtype=np.int8)
-print('train_matrix test_matrix constructed...')
 
+print("Saving data...")
 para = {'user_num': user_num, 'item_num': item_num, 'edge_index': edge_index, 'train_matrix': train_matrix,
         'test_matrix': test_matrix, 'train_ui': train_ui}
-pickle.dump(para, open('./para/movie_load.para', 'wb'))
-print('data_load finished...')
+pickle.dump(para, open('./para/movie_load.para', 'wb')) #saved to movie_load.para
+print('data loading finished...\n\n\n')
+
+
+
+######Forming Triplets......
+f_para = open('./para/movie_load.para', 'rb')
+para = pickle.load(f_para)
+user_num = para['user_num']  # total number of users
+item_num = para['item_num']  # total number of items
+train_matrix = para['train_matrix']
+train_ui = para['train_ui']
+
+print('train triple started...')
+ratio = 5  # the ratio of positive and negative
+item_ids = np.array(list(range(item_num)))
+train_triple = np.empty(shape=[0, 3], dtype=int)
+mtx = np.array(train_matrix.todense())
+para_index = 0
+for lh, inter in enumerate(train_ui):
+    user_id = inter[0]  # user_id is an 1-D numpy array
+    bool_index = ~np.array(mtx[user_id, :], dtype=bool)
+    can_item_ids = item_ids[bool_index]  # the id list of 0-value items
+    a1 = np.random.choice(can_item_ids, size=ratio, replace=False)  # a1 is an 1-D numpy array
+    inter = np.expand_dims(inter, axis=0)
+    inter = np.repeat(inter, repeats=ratio, axis=0)
+    a1 = np.expand_dims(a1, axis=1)
+    triple = np.append(inter, a1, axis=1)
+    train_triple = np.append(train_triple, triple, axis=0)
+    if lh % 10000 == 9999:
+        print('%d completed.....' % (lh + 1))
+    if lh % 3e4 == (3e4 - 1) and lh < len(train_ui)-1:
+        train_i = train_triple[:, 0]
+        train_j = train_triple[:, 1] + user_num
+        train_m = train_triple[:, 2] + user_num
+        para = {'train_i': train_i, 'train_j': train_j, 'train_m': train_m}
+
+        #saving data to movie_triple
+        pickle.dump(para, open('./para/movie_triple_' + str(para_index) + '.para', 'wb'))
+        print('para'+str(para_index)+' saved...')
+
+        train_triple = np.empty(shape=[0, 3], dtype=int)
+        para_index += 1
+        del para
+        gc.collect()
+
+train_i = train_triple[:, 0]
+train_j = train_triple[:, 1] + user_num
+train_m = train_triple[:, 2] + user_num
+para = {'train_i': train_i, 'train_j': train_j, 'train_m': train_m}
+pickle.dump(para, open('./para/movie_triple_'+str(para_index)+'.para', 'wb'))
+print('data triplets finished...')
+
